2c422965f7684adf613d3e24c650a9811aea0eed
Bbic123 <134294775+Bbic123@users.noreply.github.com>
2023-08-07 10:48:17 +0800

    123

--- ZJ_d067_1.cpp
+++ ZJ_d067_1.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main(){
     int y;
     cin >> y;
-    if(y % 400 == 0){
+    if(y % 400 == 0){ // 浪琩琌400计
         cout << "a leap year\n";
     }
-    else if(y % 4 == 0){
+    else if(y % 4 == 0){ // 琌4计
         if(y % 100 != 0){
-            cout << "a leap year\n";
+            cout << "a leap year\n"; // 璝ぃ100计秥
         }
         else{
-            cout << "a normal year\n";
+            cout << "a normal year\n"; // 玥キ
         }
     }
     else{
-        cout << "a normal year\n";
+        cout << "a normal year\n"; // 逞常琌キ
     }
 }
