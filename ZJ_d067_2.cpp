10fa00048c8f7793b388df056d73b5ae8a8dd084
Bbic123 <134294775+Bbic123@users.noreply.github.com>
2023-08-07 11:54:25 +0800

    123

---
+++ ZJ_d067_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int y;
+    cin >> y;
+    if(y % 400 == 0){
+        cout << "a leap year\n";
+    }
+    else if(y % 4 == 0 && y % 100 != 0){
+        cout << "a leap year\n";
+    }
+    else{
+        cout << "a normal year\n";
+    }
+}
