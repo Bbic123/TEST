9cad669e4f4370bae74cdbb399d163fce3496dd5
Bbic123 <134294775+Bbic123@users.noreply.github.com>
2023-08-07 15:43:31 +0800

    123

--- ZJ_d067_3.cpp
+++ ZJ_d067_3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int y;
     cin >> y;
-    if(y % 400 == 0 || (y % 100 != 0 && y % 4 == 0)){ // å†åˆä½µ
+    if(y % 400 == 0 || (y % 100 != 0 && y % 4 == 0)){ // ¦A¦X¨Ö
         cout << "a leap year\n";
     }
     else{
