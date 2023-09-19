543f5c5fd67a730421b6a79bf89ebca0e634b3a2
Bbic123 <134294775+Bbic123@users.noreply.github.com>
2023-09-19 22:28:01 +0800

    uwu

---
+++ ZJ/ZJ_f815.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+const int N = 2e4 + 5;
+int a[N];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    ll c, c1;
+    cin >> n >> c;
+    for(int i = 0; i < n; ++i) cin >> a[i];
+    ll l = 0, r = 3e9 + 1, mid;
+    while(l < r){
+        c1 = c;
+        mid = (l + r) >> 1;
+        for(int i = 0; i < n; ++i){
+            if(a[i] < mid) c1 -= (mid - a[i]) * (mid - a[i]);
+            if(c1 < 0) break;
+        }
+        if(c1 < 0) r = mid;
+        else l = mid + 1;
+    }
+    cout << r - 1 << '\n';
+}
