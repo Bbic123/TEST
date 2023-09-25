7bb78a2f7a1c41d063b23ce3999babae269458fb
Bbic123 <134294775+Bbic123@users.noreply.github.com>
2023-09-25 18:02:37 +0800

    abd

---
+++ AtCoder/abc230_pE.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main(){ // abc230_pE
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll n, ans = 0, m, n1;
+    cin >> n;
+    for(ll i = 1; i <= n; ++i){
+        m = n / i;
+        if(m < i){
+            ans += m * ((n / m) - i + 1);
+            i = (n / m);
+        }
+        else ans += m;
+    }
+    cout << ans << '\n';
+}
