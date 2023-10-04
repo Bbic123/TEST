c81d31ab20e95152ce5c3594c9f02890e8d51a08
Bbic123 <134294775+Bbic123@users.noreply.github.com>
2023-10-05 01:12:35 +0800

    1618

---
+++ TIOJ/TIOJ_1618.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+const int N = 5e5 + 5;
+int h[N], b[N];
+deque<int> dq;
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, k, ans = -2147483648, ansp, now = 0;
+    cin >> n >> k;
+    for(int i = 1; i <= n; ++i) cin >> h[i];
+    for(int i = 1; i <= n; ++i){
+        cin >> b[i]; // input bi
+        while(!dq.empty() && h[dq.back()] <= h[i]){ // back
+            now -= b[dq.back()];
+            dq.pop_back();
+        }
+        dq.push_back(i), now += b[i];
+        if(!dq.empty() && dq.front() < i - k + 1){ // front
+            now -= b[dq.front()];
+            dq.pop_front();
+        }
+        if(now > ans){ // ans
+            ans = now;
+            ansp = i;
+        }
+    }
+    cout << ansp << ' ' << ans << '\n';
+}
