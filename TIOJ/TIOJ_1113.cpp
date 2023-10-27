24b76970b4d817a04fdd63edf18c029f1dd9274f
Bbic123 <134294775+Bbic123@users.noreply.github.com>
2023-10-27 16:31:27 +0800

    1113

---
+++ TIOJ/TIOJ_1113.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    while(cin >> s){
+        sort(s.begin(), s.end());
+        do{
+            cout << s << '\n';
+        }while(next_permutation(s.begin(), s.end()));
+        cout << '\n';
+    }
+    return 0;
+}
