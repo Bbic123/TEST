c282b2dc59f7d992a7ae6be498f55bf97a7065f4
Bbic123 <134294775+Bbic123@users.noreply.github.com>
2023-12-09 01:16:00 +0800

    1209

---
+++ TIOJ/TIOJ_1209.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 4e4 + 5;
+
+vector<int> ver[N];
+short color[N];
+bool ok;
+
+void dfs(int x){
+	for(int i : ver[x]){
+		if(color[i] == -1){
+			color[i] = color[x] ^ 1;
+			dfs(i);
+		}
+		else if(color[i] == color[x]){
+			ok = false;
+			return;
+		}
+	}
+}
+
+signed main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, m;
+	while(cin >> n >> m, n){
+		for(int i = 1; i <= n; ++i){
+			ver[i].clear();
+			color[i] = -1;
+		}
+		for(int i = 0, a, b; i < m; ++i){
+			cin >> a >> b;
+			ver[a].push_back(b);
+			ver[b].push_back(a);
+		}
+		ok = true;
+		color[1] = 0;
+		dfs(1);
+		cout << (ok ? "Yes\n" : "No\n");
+	}
+	return 0;
+}
