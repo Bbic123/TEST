d334ccb470decb3b39d7d3cf59a1c3b3db68fb0b
Bbic123 <134294775+Bbic123@users.noreply.github.com>
2023-12-10 18:52:57 +0800

    BIT

---
+++ ZJ/ZJ_f315.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+const int N = 1e5 + 5;
+int BIT[N], arr[N], n;
+
+#define lowbit(x) ((x) & (-x))
+
+void update(int idx){
+	while(idx <= n){
+		BIT[idx] += 1;
+		idx += lowbit(idx);
+	}
+	return;
+}
+
+int query(int idx){
+	ll re = 0;
+	while(idx){
+		re += BIT[idx];
+		idx -= lowbit(idx);
+	}
+	return re;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int num, que;
+	ll ans = 0;
+	cin >> n;
+	for(int i = 0; i < (n << 1); ++i){
+		cin >> num;
+		update(num);
+		que = query(num - 1);
+		ans += que - (arr[num] << 1);
+		arr[num] = que;
+	}
+	cout << ans << '\n';
+	return 0;
+}
