2ab945efee7f3a57075f0d614595fa027cbc4856
Bbic123 <134294775+Bbic123@users.noreply.github.com>
2024-03-20 23:05:14 +0800

    floor

--- AtCoder/abc230_E.cpp
+++ AtCoder/abc230_E.cpp
@@ -6,15 +6,12 @@ typedef long long ll;
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	ll n, ans = 0, m, n1;
+	ll n, ans = 0, m;
 	cin >> n;
 	for(ll i = 1; i <= n; ++i){
 		m = n / i;
-		if(m < i){
-			ans += m * ((n / m) - i + 1);
-			i = (n / m);
-		}
-		else ans += m;
+		ans += m * ((n / m) - i + 1);
+		i = (n / m);
 	}
 	cout << ans << '\n';
 	return 0;
