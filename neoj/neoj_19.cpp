8ccf8585fcedf1e73ddf95a653129210e9a3bc70
Bbic <134294775+Bbic123@users.noreply.github.com>
2025-03-13 20:27:22 +0800

    ok

--- neoj/neoj_19.cpp
+++ neoj/neoj_19.cpp
@@ -4,23 +4,23 @@ using  namespace std; // 19. 大善人老闆救濟東南亞兒童
 stack<int> sk;
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t, n, num, now;
-    cin >> t;
-    while(t--){
-        now = 1;
-        cin >> n;
-        for(int i = 1; i <= n; ++i){
-            cin >> num;
-            while(now <= num){
-                sk.push(now);
-                ++now;
-            }
-            while(!sk.empty() && sk.top() == num) sk.pop();
-        }
-        cout << (sk.empty() ? "Yes\n" : "No\n");
-        while(!sk.empty()) sk.pop();
-    }
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t, n, num, now;
+	cin >> t;
+	while(t--){
+		now = 1;
+		cin >> n;
+		for(int i = 1; i <= n; ++i){
+			cin >> num;
+			while(now <= num){
+				sk.push(now);
+				++now;
+			}
+			while(!sk.empty() && sk.top() == num) sk.pop();
+		}
+		cout << (sk.empty() ? "Yes\n" : "No\n");
+		while(!sk.empty()) sk.pop();
+	}
+	return 0;
 }
